db38155ea44ca43fedd0217676024fc9959d3130
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2025-09-05 06:21:41 +0000

    Update fs2-core, fs2-io to 3.12.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     val scodec1 = "1.11.10"
     val scodec2 = "2.2.2"
     val catsCore = "2.9.0"
-    val fs2 = "3.12.0"
+    val fs2 = "3.12.2"
     val scalaCheck = "1.17.0"
     val scalaCsv = "1.3.10"
     val postgres = "42.7.0"
